3fffacfe5dbd9a39519f695ac79fd703e6790db8
eve.cs <eve.chunyanan.s@gmail.com>
2025-09-01 21:57:13 +0700

    feat:now have models of Item.js and User.js

--- api/models/Item.js
+++ api/models/Item.js
@@ -4,15 +4,15 @@ const ItemScheme = new Schema(
   {
     productname: { type: String, required: true },
     description: { type: String, default: "" },
-    price: {type: Number, default: 0, required: true},
-    image: {type: String, default:""},
-    color: {type:[String, Number], default: ["white",0]},
-    stock: {type: Number, default:0, required: true},
-    brand: {type: String, default:""},
-    features: {type: [String], default: []},
-    modelname: {type: String, default:""},
-    warrantyinfo: {type: [String, Number], default: ["",0]},,
-    relatedproduct:{type: [String], default: []}, //to show other items that user might interest
+    price: { type: Number, default: 0, required: true },
+    image: { type: String, default: "" },
+    color: { type: [String, Number], default: ["white", 0] },
+    stock: { type: Number, default: 0, required: true },
+    brand: { type: String, default: "" },
+    features: { type: [String], default: [] },
+    modelname: { type: String, default: "" },
+    warrantyinfo: { type: [String, Number], default: [" ", 0] },
+    relatedproduct: { type: [String], default: [] }, //to show other items that user might interest
   },
   { timestamps: true } //use this one because it's show a create time and edit time
 );
