5a7ad847fe4f6a2db543d8339e55201be21d0bbe
G0lf-Thanap0n <thanaponphunthawi@gmail.com>
2025-09-08 22:03:07 +0700

    feat(User.js)add role and cart field

--- api/models/User.js
+++ api/models/User.js
@@ -2,12 +2,24 @@ import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
   {
-    name: { type: String, require: true },
-    lastname: { type: String, require: true },
-    username: { type: String, default: "" },
-    email: { type: String, require: true },
-    password: { type: String, require: true },
-    cfpassword: { type: String, require: true },
+    user_name: { type: String, require: true },
+    user_lastname: { type: String, require: true },
+    user_username: { type: String, default: "" },
+    user_email: { type: String, require: true },
+    user_password: { type: String, require: true },
+    user_cfpassword: { type: String, require: true },
+    user_role: { type: String, enum: ["User", "Admin"], default: "User" },
+    user_cart: [
+      {
+        product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        product_name: { type: String, require: true },
+        product_image: { type: String, require: true },
+        product_color: { type: String, require: true },
+        product_price: { type: Number, require: true },
+        product_qty: { type: Number, require: true },
+        addedOn: { type: Date, default: new Date().getTime() },
+      },
+    ],
   },
   { timestamps: true }
 );
