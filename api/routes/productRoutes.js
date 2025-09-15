aa890f39dfdbc9033a0f7f3ee7ecd6d75d0fb29d
eve.cs <eve.chunyanan.s@gmail.com>
2025-09-16 04:14:02 +0700

    feat:productRoutes.js work now

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,1 +1,34 @@
+import { dotenv } from "dotenv";
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  editProduct,
+  getAllProducts,
+} from "../controllers/productsControllers";
+import { authUser } from "../../middleware/auth";
 
+dotenv.config();
+
+const router = express.Router();
+
+//for all product from the website without auth
+//GET all products
+router.get("/products", getAllProducts);
+//POST a new product in database
+router.post("/products", createProduct);
+
+//Add Product by auth admin only
+router.post("/add-product", authUser, createProduct);
+
+//Edit Product
+router.put("edit-product/:productId", authUser, editProduct);
+
+//Delete Product
+router.delete("/delete-product/:productId", authUser, deleteProduct);
+
+//Below this are feature that we still don't have
+//User add favorite
+//router.put("/update-product-fav/:productId", authUser, togglefav)
+
+export default router;
