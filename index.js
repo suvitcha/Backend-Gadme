bdab39813d567f4f319a97e3573d5e88d9b63d0c
suvitcha <boom.suvitcha@gmail.com>
2025-09-17 13:51:19 +0700

    upcors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://gadme-ecommerce-frontend.vercel.app",
+    "https://gadme-ecommerce-three.vercel.app",
   ], // frontend domain
   credentials: true,
 };
